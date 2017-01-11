bf67243abb7eabd3fef272db06e031fa69606511
Evan McCoy <qwertynessthedev@gmail.com>
2017-01-11 08:15:26 -0500

    Deleted extreneous files.

--- src/org/usfirst/frc/team4564/robot/Robot.java
+++ src/org/usfirst/frc/team4564/robot/Robot.java
@@ -1,14 +1,14 @@
 package org.usfirst.frc.team4564.robot;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
-//import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class Robot extends SampleRobot {
-	//public static XboxController j = new XboxController(0);
+	public static XboxController j = new XboxController(0);
 	public static NetworkTable table;
 	public Thrower thrower;
 	
