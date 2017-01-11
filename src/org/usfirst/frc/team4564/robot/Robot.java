3d8a9fc2e59da1855cb4f0f19fd617c57d0e835d
Steve <SteveJacobs@iname.com>
2017-01-10 21:23:16 -0500

    Testing joystick

--- src/org/usfirst/frc/team4564/robot/Robot.java
+++ src/org/usfirst/frc/team4564/robot/Robot.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4564.robot;
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
@@ -7,18 +8,24 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
-
+	public static XboxController j = new XboxController(0);
+	
     public Robot() {
     }
     
     public void robotInit() {
+
     }
 
     public void autonomous() {
     	
     }
     public void operatorControl() {
-       
+    	while (isOperatorControl() && isEnabled()) {
+    		if (j.getAButton()) {
+    			SmartDashboard.putBoolean("A Button", j.getAButton());
+    		}
+    	}
     }
     public void test() {
     }
