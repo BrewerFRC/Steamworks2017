19834ec25ec0f634f9c18940219889a77e775a15
Steve <SteveJacobs@iname.com>
2017-02-02 18:06:42 -0500

    updating worspace to repository

--- src/org/usfirst/frc/team4564/robot/DriveTrain.java
+++ src/org/usfirst/frc/team4564/robot/DriveTrain.java
@@ -1,11 +1,14 @@
 package org.usfirst.frc.team4564.robot;
 
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 
 public class DriveTrain extends RobotDrive {
+	CANTalon talon = new CANTalon(0);
+	
 	private static Heading heading;
-
 	private static final Talon FrontL = new Talon(Constants.PWM_DRIVE_FL);
 	private static final Talon FrontR = new Talon(Constants.PWM_DRIVE_FR);
 	private static final Talon BackL = new Talon(Constants.PWM_DRIVE_BL);
@@ -18,6 +21,16 @@ public class DriveTrain extends RobotDrive {
 		heading = new Heading(Heading.P, Heading.I, Heading.D);
 	}
 	
+	public void init() {
+		talon.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		talon.reverseSensor(false);
+		talon.setProfile(0);
+        talon.setF(1);
+        talon.setP(2);
+        talon.setI(3); 
+        talon.setD(4);
+      
+	}
     public void setDrive(double drive, double turn, double slide) {
     	slide = slide * 0.6;
     	turn = turn * 0.85;
--- src/org/usfirst/frc/team4564/robot/Robot.java
+++ src/org/usfirst/frc/team4564/robot/Robot.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
 
 public class Robot extends SampleRobot {
 	private static Robot instance;
@@ -18,6 +20,8 @@ public class Robot extends SampleRobot {
 	
 	private boolean prevRightTrigger;
 	
+	CANTalon talon = new CANTalon(1);
+	
     public Robot() {
     	instance = this;
     	dt = new DriveTrain();
@@ -25,10 +29,11 @@ public class Robot extends SampleRobot {
     	gearVision = new GearVision();
     	j = new Xbox(0);
     	prevRightTrigger = false;
+    	dt.init();
     }
     
     public void robotInit() {
-
+    	
     }
 
     public void autonomous() {
@@ -41,6 +46,7 @@ public class Robot extends SampleRobot {
 	    double flywheelSpeed = -0.85;
     	while (isEnabled() && isOperatorControl()) {
     		time = Common.time();
+    		talon.set(.3);
     		if(j.when("rightTrigger")) {
     			if(prevRightTrigger == false) {
     				thrower.state.fire();
@@ -113,11 +119,13 @@ public class Robot extends SampleRobot {
     			gearVision.reset();
     			dt.setDrive(forward, turn, slide);
     		}
-    		
+
     		//End of Xbox tests
     		SmartDashboard.putNumber("encoder", thrower.encoder.get());
     		SmartDashboard.putNumber("rpm", thrower.getRPM());
     		SmartDashboard.putNumber("samplesToAverage", thrower.encoder.getSamplesToAverage());
+    		SmartDashboard.putNumber("Encoder Value", talon.getEncPosition());
+    		SmartDashboard.putNumber("Encoder Rate", (talon.getEncVelocity()));
     	   thrower.state.update();
     	   thrower.update();
     	   double delay = (1000.0/Constants.REFRESH_RATE - (Common.time() - time)) / 1000.0;
@@ -133,6 +141,7 @@ public class Robot extends SampleRobot {
     public void disabled() {
     	j.setRumble(RumbleType.kLeftRumble, 0);
 		j.setRumble(RumbleType.kRightRumble, 0);
+		talon.setPosition(0);
     }
     
     public static DriveTrain getDriveTrain() {
